f45d5771fc876d7d2080ae030c643dc612f7dfac
Wish Patabendi <wpatabendi@opsecsecurityonline.com>
2022-04-04 01:24:13 +0100

    Implement discount method for buy Two for one pound

--- src/main/java/kata/supermarket/TwoForOnePoundDiscount.java
+++ src/main/java/kata/supermarket/TwoForOnePoundDiscount.java
@@ -13,6 +13,13 @@ public class TwoForOnePoundDiscount implements Discount {
 
     @Override
     public BigDecimal discount(List<Item> items) {
-        return new BigDecimal("0.30");
+        // eligible items for discounts
+        int noOfItemsForDiscount = items.size() / 2;
+
+        // discount for 2 = 2 items price - £1
+        BigDecimal discountForTwoItems = itemAmount.multiply(BigDecimal.valueOf(2)).subtract(BigDecimal.valueOf(1.00));
+
+        //discount for all eligible items
+        return discountForTwoItems.multiply(BigDecimal.valueOf(noOfItemsForDiscount));
     }
 }
